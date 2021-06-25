0d605e3889850ba14e47416a8f39dea10bc52646
Your Name <you@example.com>
2021-06-25 20:51:40 +0530

    update

---
+++ Frontend/src/Editor.js
@@ -0,0 +1,25 @@
+import React,{ useState } from 'react'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import parse from 'html-react-parser';
+
+function Editor() {
+    const [value, setValue] = useState("");
+
+    const handleOnChange = (e, editor) => {
+        const data = editor.getData()
+        setValue(data)
+    }
+    return (
+        <>
+        <div>
+            <CKEditor editor={ClassicEditor} onChange={handleOnChange} />
+        </div>
+        <div>
+            {parse(value)}
+        </div>
+        </>
+    )
+}
+
+export default Editor
